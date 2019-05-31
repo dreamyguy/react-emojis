8ed05156612a22ad3dc8c1765eee109e2a871a16
Wallace Sidhrée <i@dreamyguy.com>
2019-05-31 23:24:14 +0200

    Updates

---
+++ precache-manifest.367d969a97df5624f9c44d633afb24f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918b6a4e8e0b19a849146d5030f789f9",
+    "url": "/react-emojis/index.html"
+  },
+  {
+    "revision": "d0ffdaf7247809d10f45",
+    "url": "/react-emojis/static/css/main.b9e37371.chunk.css"
+  },
+  {
+    "revision": "fad91580a399014f47c9",
+    "url": "/react-emojis/static/js/2.b763c512.chunk.js"
+  },
+  {
+    "revision": "d0ffdaf7247809d10f45",
+    "url": "/react-emojis/static/js/main.b8cf3f63.chunk.js"
+  },
+  {
+    "revision": "16f63e68caae6a5657c8",
+    "url": "/react-emojis/static/js/runtime~main.0ebe51d8.js"
+  }
+]);
