fe3f504b0a8b96e5fe5087912a090bf467e840fa
Wallace Sidhrée <i@dreamyguy.com>
2019-06-01 11:33:32 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-emojis/precache-manifest.4b9333a30cc7590a50902ec77ac71679.js"
+  "/react-emojis/precache-manifest.67fdf19d2e890735e217c0e60bfd9f80.js"
 );
 
 self.addEventListener('message', (event) => {
